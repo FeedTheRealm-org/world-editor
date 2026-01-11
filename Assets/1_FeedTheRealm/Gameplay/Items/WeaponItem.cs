ac0690881f6167d58d13fd7160bfba4fc6539ae4
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-01-11 20:22:20 -0300

    feat: created WeaponItem for gameplay

---
+++ Assets/1_FeedTheRealm/Gameplay/Items/WeaponItem.cs
@@ -0,0 +1,33 @@
+using Models;
+
+public class WeaponItem : Item
+{
+    public WeaponType weaponType;
+    public int damage;
+    public float attackSpeed;
+    public float range;
+    public int ammo;
+
+    public WeaponItem(WeaponItemData weaponItemData)
+        : base(weaponItemData)
+    {
+        weaponType = weaponItemData.weaponType;
+        damage = weaponItemData.damage;
+        attackSpeed = weaponItemData.attackSpeed;
+        range = weaponItemData.range;
+        ammo = weaponItemData.ammo;
+    }
+
+    public override void SaveObject(ref WorldData worldData)
+    {
+        WeaponItemData weaponItemData = itemDataBuilder
+            .SetItemData(ObjectId, DisplayName, description, spriteFile)
+            .BuildWeaponItem(weaponType, damage, attackSpeed, range, ammo);
+        worldData.weaponItems.Add(weaponItemData);
+    }
+
+    public override void DeleteObject(ref WorldData worldData)
+    {
+        worldData.weaponItems.RemoveAll(item => item.id == ObjectId);
+    }
+}
