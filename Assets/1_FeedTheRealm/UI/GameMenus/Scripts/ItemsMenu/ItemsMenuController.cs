e2e89c05187de62ca9f8d06b7a3a71a95340cf37
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-01-12 17:06:51 -0300

    feat: created CreatorObjectCategoriesExtension for associte each category to string

--- Assets/1_FeedTheRealm/UI/GameMenus/Scripts/ItemsMenu/ItemsMenuController.cs
+++ Assets/1_FeedTheRealm/UI/GameMenus/Scripts/ItemsMenu/ItemsMenuController.cs
@@ -56,7 +56,7 @@ public class ItemsMenuController : MenuController
                 headerLabel.text = item.DisplayName;
 
                 var typeLabel = itemEntry.Q<Label>("Type");
-                typeLabel.text = category.ToString();
+                typeLabel.text = category.GetDisplayName();
 
                 var editButton = itemEntry.Q<Button>("Edit");
                 var deleteButton = itemEntry.Q<Button>("Delete");
