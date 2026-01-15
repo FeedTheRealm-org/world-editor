8506687c319524a54ce8f28b4138b099e82681e8
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-01-15 12:17:08 -0300

    feat: added NPCSpawner object for world editor

---
+++ Assets/1_FeedTheRealm/Gameplay/Spawners/Scripts/NPCSpawnerController.cs
@@ -0,0 +1,84 @@
+using Models;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class NPCSpawnerController : SpawnerController, IPersistent, ISelectable
+{
+    [SerializeField]
+    private MakerInputReader inputReader;
+    private UIDocument editorMenu;
+    private Slider radiusSlider;
+    private Button closeButton;
+    private NPCSpawnerData _npcSpawnData;
+
+    public NPCSpawnerData NPCSpawnData
+    {
+        get { return _npcSpawnData; }
+        set
+        {
+            _npcSpawnData = value;
+            transform.position = _npcSpawnData.Position;
+            transform.localScale = new Vector3(
+                _npcSpawnData.Radius,
+                transform.localScale.y,
+                _npcSpawnData.Radius
+            );
+        }
+    }
+
+    void OnEnable()
+    {
+        NPCSpawnData = new NPCSpawnerData(transform.position, transform.localScale.x);
+        editorMenu = GetComponent<UIDocument>();
+        var root = editorMenu.rootVisualElement;
+
+        radiusSlider = root.Q<Slider>("SpawnerRadius");
+        closeButton = root.Q<Button>("Close");
+
+        radiusSlider.value = NPCSpawnData.Radius;
+
+        radiusSlider.RegisterValueChangedCallback(e =>
+        {
+            NPCSpawnData.Radius = e.newValue;
+            transform.localScale = new Vector3(e.newValue, transform.localScale.y, e.newValue);
+        });
+        closeButton.clicked += OnCloseClicked;
+
+        editorMenu.rootVisualElement.style.display = DisplayStyle.None;
+    }
+
+    void OnDisable()
+    {
+        closeButton.clicked -= OnCloseClicked;
+    }
+
+    private void RenderMenu()
+    {
+        inputReader.ToggleInput(false);
+        radiusSlider.value = NPCSpawnData.Radius;
+
+        editorMenu.rootVisualElement.style.display = DisplayStyle.Flex;
+    }
+
+    private void OnCloseClicked()
+    {
+        Debug.Log("EnemySpawnerController: Closing editor menu");
+        inputReader.ToggleInput(true);
+        editorMenu.rootVisualElement.style.display = DisplayStyle.None;
+    }
+
+    public void OnObjectSelected()
+    {
+        RenderMenu();
+    }
+
+    public override void SaveData(ref WorldData worldData)
+    {
+        if (!gameObject.activeSelf)
+            return;
+
+        _npcSpawnData.Position = transform.position;
+        worldData.npcSpawnAreas.Add(NPCSpawnData);
+    }
+}
