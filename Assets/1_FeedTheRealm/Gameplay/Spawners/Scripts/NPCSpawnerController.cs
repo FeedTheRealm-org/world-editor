59e0d82a18f05c50623f1ce18862c6de8fd0ed2b
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-01-19 00:52:35 -0300

    feat: added missing `IEditable` to NPCSpawnerController

--- Assets/1_FeedTheRealm/Gameplay/Spawners/Scripts/NPCSpawnerController.cs
+++ Assets/1_FeedTheRealm/Gameplay/Spawners/Scripts/NPCSpawnerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(UIDocument))]
-public class NPCSpawnerController : SpawnerController, IPersistent
+public class NPCSpawnerController : SpawnerController, IPersistent, IEditable
 {
     [SerializeField]
     private MakerInputReader inputReader;
